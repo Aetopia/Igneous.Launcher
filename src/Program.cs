e4f507edc19d0d3791d0f0cdeba7c0dcae3d3575
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-05-30 16:42:28 +0530

    Add a bunch of comments.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using Igneous.Launcher.Minecraft;
 
 using static Igneous.Launcher.PInvoke.Shell32;
 using static Igneous.Launcher.PInvoke.Constants;
+using System.Threading;
 
 static class Program
 {
@@ -14,8 +15,11 @@ static class Program
         AppDomain.CurrentDomain.UnhandledException += (_, args) =>
         {
             ShellMessageBox(0, 0, $"{args.ExceptionObject}", "Error", MB_ICONERROR);
-            Environment.Exit(default);
+            Environment.Exit(0);
         };
+
+        using Mutex mutex = new(false, "F3C6CA66-40CC-4E9E-AE10-F11064726AFC", out var createdNew);
+        if (!createdNew) return;
 
         Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
         Loader.Launch([.. Settings.Startup]);
