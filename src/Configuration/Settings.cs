05e986947ce59ff6c3ada142a4b4e5593c417f2c
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-05-30 16:45:43 +0530

    Rename files.

--- src/Configuration/Settings.cs
+++ src/Configuration/Settings.cs
@@ -10,13 +10,13 @@ sealed class Settings
 
     static HashSet<string> _runtime;
 
-    internal static HashSet<string> Startup => _startup ??= Serializer.Load("Startup.json");
+    internal static HashSet<string> Startup => _startup ??= Serializer.Load("Igneous.Launcher.Startup.json");
 
-    internal static HashSet<string> Runtime => _runtime ??= Serializer.Load("Runtime.json");
+    internal static HashSet<string> Runtime => _runtime ??= Serializer.Load("Igneous.Launcher.Runtime.json");
 
     internal static void Save()
     {
-        Serializer.Save("Startup.json", _startup);
-        Serializer.Save("Runtime.json", _runtime);
+        Serializer.Save("Igneous.Launcher.Startup.json", _startup);
+        Serializer.Save("Igneous.Launcher.Runtime.json", _runtime);
     }
 }
